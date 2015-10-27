a717be21efaa8a0f938522bb5a60e59030a18d4b
Saziya Latif <saziyalatif@gmail.com>
2015-10-27 14:29:47 -0700

    jquery code

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,20 @@ function submitButton () {
 	alert ("Thank you for submitting your thoughts.")
 }
 
+// Starting jquery
+$(document).ready (function() {
+//hide the images
+$("foodpictures").hide();	
+
+//make slides of pictures
+$("foodpictures").click (function){
+	$(this).next().slideToggle(300);
+
+	});
+});
+
+
+
 
 
 
