c0482c34a41e5c0b57b7b104a0104bb91524bfc7
Saziya Latif <saziyalatif@gmail.com>
2015-11-18 10:51:49 -0800

    pictures

--- scripts/server.js
+++ scripts/server.js
@@ -1,3 +1,4 @@
+
 var http = require ("http");
 
 http.createServer(function (request, response) {
@@ -7,8 +8,27 @@ http.createServer(function (request, response) {
 }).listen(3000);
 
 /*
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
 */
